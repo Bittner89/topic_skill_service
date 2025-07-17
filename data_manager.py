675d82ece3baae9a0a52c11f2da62ec9965289ec
Bittner89 <bittner-89@web.de>
2025-07-17 14:30:19 +0200

    Kommentierung der data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,18 +1,22 @@
 import json
 import os
 
-class JsonDataManager:
+class JsonDataManager:  #Eine Klasse zum Verwalten von JSON-Daten, die das Lesen aus und Schreiben in JSON-Dateien vereinfacht und Fehlerbehandlung integriert.
 
     def __init__(self):
+        #Der Konstruktor der Klasse. Aktuell ist er leer, da für diese Funktionalität keine Initialisierungsparameter oder Instanzvariablen benötigt werden.
         pass
 
 
 
-    def read_data(self, filepath):
+    def read_data(self, filepath):          #Liest Daten aus einer JSON-Datei.
         if not os.path.exists(filepath):
             return []
+        
+        # Überprüft, ob die angegebene Datei existiert.
+        # Wenn nicht, gibt die Methode eine leere Liste zurück, da es nichts zu lesen gibt.
     
-        try:
+        try:                                                            #Erstellung eines try und except blocks. Um Fehler abzufangen.
             with open(filepath, 'r', encoding='utf-8') as file:
                 return json.load(file)
         except json.JSONDecodeError:
@@ -23,11 +27,13 @@ class JsonDataManager:
             return []
 
 
-    def write_data(self, filepath, data):
+    def write_data(self, filepath, data):       #Schreibt Daten in eine JSON-Datei.
+                                                #Erstellt bei Bedarf die Verzeichnisse im Dateipfad.
 
-        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)       # Erstellt alle notwendigen Verzeichnisse im Pfad zur Datei, falls sie noch nicht existieren.
+                                                                    # 'exist_ok=True' verhindert einen Fehler, wenn die Verzeichnisse bereits vorhanden sind.
 
-        try:
+        try:                                                        #Erstellung eines try und except blocks. Um Fehler abzufangen.
             with open(filepath, 'w', encoding='utf-8') as file:
                 json.dump(data, file, indent=4)
                 return True
