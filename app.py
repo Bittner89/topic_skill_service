d1bbbc860038ade18c9702658e41dd4bd8a097cb
Bittner89 <bittner-89@web.de>
2025-07-18 11:47:59 +0200

    PLPG-API-10: Implemented 404 status for non existing topics and skill.

--- app.py
+++ app.py
@@ -36,17 +36,21 @@ def get_skills():
 @app.route('/topics/<id>', methods=['GET'])
 def get_topic_by_id(id):
     topics = data_manager.read_data(TOPICS_FILE)
-    topic = next((topic for topic in topics if topic.get('id').lower() == id).lower(), None)
-    return jsonify(topic)
-
+    topic = next((topic for topic in topics if topic.get('id').lower() == id.lower()), None)
+    if topic:
+        return jsonify(topic)
+    else:
+        return jsonify({"error": "Topic not found."}), 404
 
 @app.route('/skills/<id>', methods=['GET'])
 def get_skill_by_id(id):
     skills = data_manager.read_data(SKILLS_FILE)
     skill = next((skill for skill in skills if skill.get('id').lower() == id.lower()), None)
-    return jsonify(skill)
+    if skill:
+        return jsonify(skill)
+    else:
+        return jsonify({"error": "Skill not found."}), 404
 
-    
 # Aufrufen der Haupt funktion
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
